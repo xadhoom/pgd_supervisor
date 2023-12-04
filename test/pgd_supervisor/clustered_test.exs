ad80a482fb6fb35411223605ff4e8649e4cfb01c
Matteo Brancaleoni <matteo.brancaleoni@voismart.it>
2023-12-04 14:33:00 +0100

    Add test for children redistribution on topology changes

--- test/pgd_supervisor/clustered_test.exs
+++ test/pgd_supervisor/clustered_test.exs
@@ -50,6 +50,39 @@ defmodule PgdSupervisor.ClusteredTest do
     end
   end
 
+  test "distributes children between nodes when topology changes" do
+    [node1, node2] = start_nodes(:test_app, "foo", 2)
+
+    child_spec_1 = Worker.child_spec(10)
+    child_spec_2 = Worker.child_spec(20)
+    child_spec_3 = Worker.child_spec(30)
+
+    {:ok, pid1} = start_child(node1, child_spec_1)
+    {:ok, pid2} = start_child(node1, child_spec_2)
+    {:ok, pid3} = start_child(node1, child_spec_3)
+
+    assert_async do
+      assert %{
+               ^node1 => [{:undefined, ^pid1, :worker, [Worker]}],
+               ^node2 => [
+                 {:undefined, ^pid2, :worker, [Worker]},
+                 {:undefined, ^pid3, :worker, [Worker]}
+               ]
+             } = local_children([node1, node2])
+    end
+
+    # now start another node and a process should migrate to it
+    [node3] = start_nodes(:test_app, "bar", 1)
+
+    assert_async do
+      assert %{
+               ^node1 => [{:undefined, ^pid1, :worker, [Worker]}],
+               ^node2 => [{:undefined, ^pid3, :worker, [Worker]}],
+               ^node3 => [{:undefined, ^pid2, :worker, [Worker]}]
+             } = local_children([node1, node2, node3])
+    end
+  end
+
   defp start_child(node, child_spec) do
     :rpc.call(node, PgdSupervisor, :start_child, [@supervisor, child_spec])
   end
